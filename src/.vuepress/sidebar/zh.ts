12b6bffd8f05ec6f0bedb2c557bc838985f34cff
zhanggong <zhanggong@58.com>
2024-03-25 19:54:26 +0800

    init

--- src/.vuepress/sidebar/zh.ts
+++ src/.vuepress/sidebar/zh.ts
@@ -4,22 +4,22 @@ export const zhSidebar = sidebar({
   "/": [
     "",
     {
-      text: "案例",
+      text: "文章",
       icon: "laptop-code",
       prefix: "demo/",
       link: "demo/",
       children: "structure",
     },
-    {
-      text: "文档",
-      icon: "book",
-      prefix: "guide/",
-      children: "structure",
-    },
-    {
-      text: "幻灯片",
-      icon: "person-chalkboard",
-      link: "https://plugin-md-enhance.vuejs.press/zh/guide/content/revealjs/demo.html",
-    },
+    // {
+    //   text: "文档",
+    //   icon: "book",
+    //   prefix: "guide/",
+    //   children: "structure",
+    // },
+    // {
+    //   text: "幻灯片",
+    //   icon: "person-chalkboard",
+    //   link: "https://plugin-md-enhance.vuejs.press/zh/guide/content/revealjs/demo.html",
+    // },
   ],
 });
